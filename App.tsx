0015031b1eb09f758db012f7092d31aa3124fbcb
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-02-15 14:07:37 -0300

    configuring navigation

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <View>
       <StatusBar backgroundColor={"#1d1d2e"} barStyle="light-content" translucent={false} />
-
       <SignIn />
     </View>
   );
