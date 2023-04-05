d30d246d334972ac84481b815b9b3639a4297da7
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-04-05 20:29:02 -0300

    checkout screen

---
+++ src/pages/FinishOrder/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+    View,
+    Text,
+    TouchableOpacity
+} from 'react-native';
+
+import { styles } from './styles';
+
+export default function FinishOrder() {
+
+
+    return (
+
+        <View style={styles.container}>
+            <Text>
+                Tela finish
+            </Text>
+        </View>
+    )
+}
---
+++ src/pages/FinishOrder/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1d1d2e',
+        paddingVertical: '5%',
+        paddingHorizontal: '4%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+});
